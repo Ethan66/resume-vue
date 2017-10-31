7f87d1d9812b787532da01e7107e45b4e627b9e8
Ethan66 <2064281269@qq.com>
2017-10-31 16:10:35 +0800

    增加skill组件

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -13,7 +13,9 @@
         <ArrayEditor v-bind:items="resume.education" v-bind:title="{zero:'学校',one:'学校名称',two:'学位',three:'专业',four:'日期',five:'在校荣誉'}"
              v-bind:textContent="'主要填写学校名，所学专业，学位以及在校荣誉'" v-bind:icons="icons[2]" />
       </li>
-      <li v-show="currentTab.tab==3">4</li>
+      <li v-show="currentTab.tab==3">
+        <Skill v-bind:skills="resume.skills" />
+      </li>
       <li v-show="currentTab.tab==4">
         <ArrayEditor v-bind:items="resume.projects" v-bind:title="{zero:'项目',one:'预览地址',two:'负责版块',three:'团队定位',four:'日期',five:'总结'}"
             v-bind:textContent="'主要填写在线项目预览、负责模块、团队定位、项目时长，以及在本项目里面的收获与体会'" v-bind:icons="icons[4]" />
@@ -26,11 +28,12 @@
   import Topbar from './Topbar'
   import Profile from './Profile'
   import ArrayEditor from './ArrayEditor'
+  import Skill from './Skill'
 
   export default{
       props:['currentTab','resume','icons'],
       components:{
-          Topbar,Profile,ArrayEditor
+          Topbar,Profile,ArrayEditor,Skill
       },
     methods:{
         addCompany(){
