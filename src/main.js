07934eaee324129d7a14d1ce5322b6f1cc020b61
Ethan66 <2064281269@qq.com>
2017-11-07 17:46:07 +0800

    添加vue-router

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'vue-router'
 import 'normalize.css'
 import './assets/reset.scss'
 
@@ -9,6 +10,7 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(ElementUI)
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
