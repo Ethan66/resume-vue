a16df39ed0bfc06270fc35ee8a5a77785fce8a55
Ethan66 <2064281269@qq.com>
2017-11-08 15:49:58 +0800

    配置路由

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import VueRouter from 'vue-router'
+import routes from './router/router-config.js'
 import 'normalize.css'
 import './assets/reset.scss'
 
@@ -12,6 +13,12 @@ import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
 Vue.use(VueRouter)
 
+const router = new VueRouter({
+  mode: 'hash',    //路由的模式，官网是没有的，应该是默认值
+  routes
+})
+
+
 Vue.config.productionTip = false
 
 
@@ -19,6 +26,7 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
   template: '<App/>',
   components: { App }
-})
+}).$mount('#app')
---
+++ src/router/router-config.js
@@ -0,0 +1,10 @@
+import ArrayEditor from '../components/ArrayEditor'
+import Editor from '../components/Editor'
+import Preview from '../components/Preview'
+import Skill from '../components/Skill'
+
+export default[
+  {
+    path:"/edit/:id",component:Editor,
+  },
+]
